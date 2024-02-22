f452d93f0f41cd93bf8a9e64350a45c613f1e66b
CJ_Felux <148596766+KenzoJ@users.noreply.github.com>
2024-02-22 01:26:33 +0000

    ready for vscode change

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import os
+#import os
+import typing
+#import json
 from discord import Intents, Client, Message, app_commands
 from replit import db
 import asyncio
-import enum
-from re import A
-import typing
-import discord 
-import json
+import discord
+from discord.ui import Button, View
+#from re import A
 from discord.ext import commands
 from references import cheat_database, stats, iv_dict, move_database
 from test_classes import export_mon
@@ -37,16 +37,6 @@ def main():
   async def list_mons(interaction: discord.Interaction):
     await interaction.response.send_message(f"All the keys: \n{db.keys()}",ephemeral = True)
 
-  '''testing functiona;ity of button
-  class InviteButtons(discord.ui.View):
-    def __init__(self, inv: str):
-        super().__init__()
-        self.inv = inv
-        self.add_item(discord.ui.Button(label="Invite link", url=self.inv))
-    @discord.ui.button(label="Invite", style=discord.ButtonStyle.red)
-    async def inviteBtn(self, interaction: discord.Interaction, button: discord.ui.Button):
-      await interaction.response.send_message(self.inv)'''
-
 # Cheat code bot
   @bot.tree.command(name="item_cheat")
   @app_commands.describe(code = "what code?")
@@ -69,13 +59,6 @@ def main():
     else:
       new_move = "no valid move name"
     await interaction.response.send_message(f"\n{move}:\n{new_move}")
-
-#Deleting pokemon 
-  #WORK IN PROGRESS
-  @bot.command()
-  async def invite(ctx: commands.Context):
-    inv = await ctx.channel.create_invite()
-    await ctx.send("Are you sure you want to delete? ", view=InviteButtons(str(inv)))
 
 #Adding a mon using discord 
 #No checks yet 
@@ -108,5 +91,15 @@ def main():
     await asyncio.sleep(5)
     lst = export_mon(need_mon)
     await interaction.followup.send(f"`\n{lst[0]}\n{lst[1]}\n{lst[2]}\n{lst[3]}\n{lst[4]}\n{lst[5]}\n- {lst[6]}\n- {lst[7]}\n- {lst[8]}\n- {lst[9]}\n`")
-  
+    
+#Deleting pokemon 
+  #WORK IN PROGRESS
+  @bot.tree.command(name="delete_mon")
+  async def delete(interaction: discord.Interaction, code: str):
+    button = Button(label="Delete?", url="https://google.com")
+    view = View()
+    view.add_item(button)
+    await ctx.send("Hi!", view=view)
+
+
 main()
